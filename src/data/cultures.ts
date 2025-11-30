5d641404976a0ad7eb4b66acb773dfa17ffd7c68
Yann Gouffon <github@yago-art.ch>
2025-11-30 16:22:01 +0100

    📝 improve cultures

--- src/data/cultures.ts
+++ src/data/cultures.ts
@@ -8,19 +8,21 @@ const lng = getLng();
 
 const items: Item[] = cultures.map((item: CSVItem) => formatCsvItem(item, lng));
 
+const nestedGroups = [
+  'cultures-africa',
+  'cultures-middle-east',
+  'cultures-europe',
+  'cultures-asia',
+  'cultures-north-america',
+  'cultures-middle-america',
+  'cultures-south-america',
+  'cultures-oceania',
+];
+
 const parent: Group = {
   id: 'cultures',
   content: t('groups.cultures.label'),
-  nestedGroups: [
-    'cultures-africa',
-    'cultures-asia',
-    'cultures-europe',
-    'cultures-middle-america',
-    'cultures-middle-east',
-    'cultures-north-america',
-    'cultures-oceania',
-    'cultures-south-america',
-  ],
+  nestedGroups,
 };
 
 const groups: Group[] = [
@@ -29,32 +31,32 @@ const groups: Group[] = [
     content: t('groups.cultures.groups.africa'),
   },
   {
-    id: 'cultures-asia',
-    content: t('groups.cultures.groups.asia'),
+    id: 'cultures-middle-east',
+    content: t('groups.cultures.groups.middle-east'),
   },
   {
     id: 'cultures-europe',
     content: t('groups.cultures.groups.europe'),
   },
   {
-    id: 'cultures-middle-america',
-    content: t('groups.cultures.groups.middle-america'),
-  },
-  {
-    id: 'cultures-middle-east',
-    content: t('groups.cultures.groups.middle-east'),
+    id: 'cultures-asia',
+    content: t('groups.cultures.groups.asia'),
   },
   {
     id: 'cultures-north-america',
     content: t('groups.cultures.groups.north-america'),
   },
   {
-    id: 'cultures-oceania',
-    content: t('groups.cultures.groups.oceania'),
+    id: 'cultures-middle-america',
+    content: t('groups.cultures.groups.middle-america'),
   },
   {
     id: 'cultures-south-america',
     content: t('groups.cultures.groups.south-america'),
+  },
+  {
+    id: 'cultures-oceania',
+    content: t('groups.cultures.groups.oceania'),
   },
 ];
 
