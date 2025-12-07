7734348a1f0ca462882bde9e05de6d90d64def54
Yann Gouffon <github@yago-art.ch>
2025-12-07 09:48:41 +0100

    🏷️ fix types

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,7 +1,7 @@
 import en from './en.json';
 import fr from './fr.json';
 
-type Locales = { [key: string]: string | Locales };
+type Locales = { [key: string]: string | Record<string, string>[] | Locales };
 
 const locales: Locales = {
   fr,
